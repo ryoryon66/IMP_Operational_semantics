27fc7349e443812619e87b6b0d9425791a48ef5c
ryoryon66 <na1729taxi@gmail.com>
2023-05-04 06:20:47 +0900

    profiler追加

--- main.py
+++ main.py
@@ -2,64 +2,19 @@ import argparse
 import time
 from copy import deepcopy
 from dataclasses import dataclass
-from typing import DefaultDict, Union
+from typing import  Union
 
-from lark import Lark, Token, Transformer, Tree
+
 from lark.lexer import Token
 from lark.tree import Tree as ParseTree
+
+from utils import  tree_to_string,constract_ast
 
 # recursion limit
 import sys
 
 sys.setrecursionlimit(10 ** 9)
     
-
-
-class RemoveRedundant(Transformer):
-    def aexp(self,children):
-        if len(children) == 1:
-            return children[0]
-        raise Exception("aexp")
-    
-    def term(self,children):
-        if len(children) == 1:
-            return children[0]
-        raise Exception("term")
-
-    def factor(self,children):
-        if len(children) == 1 and isinstance(children[0],Token):
-            return children[0]
-        raise Exception("factor")
-    
-    def bexp(self,children):
-        if len(children) == 1:
-            return children[0]
-        raise Exception("bexp")
-    
-    def band(self,children):
-        if len(children) == 1:
-            return children[0]
-        raise Exception("band")
-    
-    def bor(self,children):
-        if len(children) == 1:
-            return children[0]
-        raise Exception("bor")
-
-    def bnot(self,children):
-        if len(children) == 1:
-            return children[0]
-        raise Exception("bnot")
-    
-    def batom(self,children):
-        if len(children) == 1:
-            return children[0]
-        raise Exception("batom")
-
-    def com(self,children):
-        if len(children) == 1 and isinstance(children[0],Token):
-            return children[0]
-        raise Exception("com")
 
 
 @dataclass
@@ -193,7 +148,7 @@ class DeriviationTreeNode:
             out += anc._id + " -> " + self._id + ";\n"
         return out
     
-    def eval(self) -> Union[int,bool,Env]:
+    def eval(self) -> Union[Union[int,FunctionInfo],bool,Env]:
         
         if isinstance(self.exp, Token):
             is_aexp = self.exp.type in ["NUM","VAR"]
@@ -240,9 +195,9 @@ class DeriviationTreeNode:
             ancestor = DeriviationTreeNode(aexp,deepcopy(self.env))
             
             new_env = deepcopy(self.env)
+            self.ancestors.append(ancestor)
             new_env[var] = ancestor.eval()
             
-            self.ancestors.append(ancestor)
             self.res = new_env
             
             return new_env
@@ -252,6 +207,7 @@ class DeriviationTreeNode:
             com2 = self.exp.children[2]
             
             ancestor1 = DeriviationTreeNode(bexp,deepcopy(self.env))
+            self.ancestors.append(ancestor1)
             
             
             if ancestor1.eval():
@@ -259,7 +215,7 @@ class DeriviationTreeNode:
             else:
                 ancestor2 = DeriviationTreeNode(com2,deepcopy(self.env))
             
-            self.ancestors.append(ancestor1)
+ 
             self.ancestors.append(ancestor2)
             self.res = ancestor2.eval()
             
@@ -271,34 +227,34 @@ class DeriviationTreeNode:
             com = self.exp.children[1]
             
             ancestor1 = DeriviationTreeNode(bexp,deepcopy(self.env))
+            self.ancestors.append(ancestor1)
             
             if not ancestor1.eval():
-                self.ancestors.append(ancestor1)
                 self.res = deepcopy(self.env)
                 return deepcopy(self.env)
             else:
                 ancestor2 = DeriviationTreeNode(com,deepcopy(self.env))
-                self.ancestors.append(ancestor1)
+
                 self.ancestors.append(ancestor2)
                 env1 = ancestor2.eval()
                 ancestor3 = DeriviationTreeNode(self.exp,env1)
-                env2 = ancestor3.eval()
                 self.ancestors.append(ancestor3)
+                env2 = ancestor3.eval()
                 self.res = env2
                 return env2
         
         if data == "seq":
             ancestor1 = DeriviationTreeNode(self.exp.children[0],deepcopy(self.env))
-            ancestor2 = DeriviationTreeNode(self.exp.children[1],deepcopy(ancestor1.eval()))
-            
             self.ancestors.append(ancestor1)
+            ancestor2 = DeriviationTreeNode(self.exp.children[1],deepcopy(ancestor1.eval()))
             self.ancestors.append(ancestor2)
             self.res = ancestor2.eval()
             return self.res
         if data == "print":
             ancestor1 = DeriviationTreeNode(self.exp.children[0],deepcopy(self.env))
-            print (ancestor1.eval())
             self.ancestors.append(ancestor1)
+            line = self.exp._meta.line
+            print (f"line{line}",ancestor1.eval())
             self.res = deepcopy(self.env)
             return deepcopy(self.env)
         
@@ -344,27 +300,27 @@ class DeriviationTreeNode:
         if data == "add":
             ancestor1 = DeriviationTreeNode(self.exp.children[0],deepcopy(self.env))
             ancestor2 = DeriviationTreeNode(self.exp.children[1],deepcopy(self.env))
-            res = ancestor1.eval() + ancestor2.eval()
             self.ancestors.append(ancestor1)
             self.ancestors.append(ancestor2)
+            res = ancestor1.eval() + ancestor2.eval()
             self.res = res
             return res
         
         if data == "sub":
             ancestor1 = DeriviationTreeNode(self.exp.children[0],deepcopy(self.env))
             ancestor2 = DeriviationTreeNode(self.exp.children[1],deepcopy(self.env))
-            res = ancestor1.eval() - ancestor2.eval()
             self.ancestors.append(ancestor1)
             self.ancestors.append(ancestor2)
+            res = ancestor1.eval() - ancestor2.eval()
             self.res = res
             return res
         
         if data == "mul":
             ancestor1 = DeriviationTreeNode(self.exp.children[0],deepcopy(self.env))
             ancestor2 = DeriviationTreeNode(self.exp.children[1],deepcopy(self.env))
-            res = ancestor1.eval() * ancestor2.eval()
             self.ancestors.append(ancestor1)
             self.ancestors.append(ancestor2)
+            res = ancestor1.eval() * ancestor2.eval()
             self.res = res
             return res
         
@@ -372,33 +328,33 @@ class DeriviationTreeNode:
 
             func_name = self.exp.children[0].value # function name
             args_aexps = self.exp.children[1:] # arguments passed
-            args_ancestors = []
+            # args_ancestors = []
             
             function_info = self.env[func_name]
             env_passed = Env() # environment passed to the function
+
             for aexp, arg_name in zip(args_aexps,function_info.args):
                 arg_node = DeriviationTreeNode(aexp,self.env)
+                self.ancestors.append(arg_node)
                 evaluated_v = arg_node.eval()
-                #assert isinstance(evaluated_v,int)
-                args_ancestors.append(arg_node)
                 assert isinstance(arg_name,str)
-                #assert isinstance(evaluated_v,int)
                 env_passed[arg_name] = evaluated_v
             env_passed[func_name] = function_info #関数自身も引数に追加　再帰呼び出しのため
             
-            self.ancestors += args_ancestors #引数の評価結果をancestorsに追加
+            #self.ancestors += args_ancestors
             
             com = function_info.com
             com_node = DeriviationTreeNode(com,env_passed)
+            self.ancestors.append(com_node) #関数の本体をancestorsに追加
             env_returned = com_node.eval()
             assert isinstance(env_returned,Env)
-            self.ancestors.append(com_node) #関数の本体の評価結果をancestorsに追加
+          
             
             aexp_returned = function_info.aexp_returned
             aexp_node = DeriviationTreeNode(aexp_returned,env_returned)
+            self.ancestors.append(aexp_node) #関数の返り値をancestorsに追加
             res = aexp_node.eval()
             assert isinstance(res,int)
-            self.ancestors.append(aexp_node) #関数の返り値の評価結果をancestorsに追加
             self.res = res
             return res
             
@@ -427,194 +383,89 @@ class DeriviationTreeNode:
         if data == "and":
             ancestor1 = DeriviationTreeNode(self.exp.children[0],deepcopy(self.env))
             ancestor2 = DeriviationTreeNode(self.exp.children[1],deepcopy(self.env))
-            res = ancestor1.eval() and ancestor2.eval()
             self.ancestors.append(ancestor1)
             self.ancestors.append(ancestor2)
+            res = ancestor1.eval() and ancestor2.eval()
             self.res = res
             return res
         
         if data == "or":
             ancestor1 = DeriviationTreeNode(self.exp.children[0],deepcopy(self.env))
             ancestor2 = DeriviationTreeNode(self.exp.children[1],deepcopy(self.env))
-            res = ancestor1.eval() or ancestor2.eval()
             self.ancestors.append(ancestor1)
             self.ancestors.append(ancestor2)
+            res = ancestor1.eval() or ancestor2.eval()
             self.res = res
             return res
         
         if data == "not":
             ancestor1 = DeriviationTreeNode(self.exp.children[0],deepcopy(self.env))
-            res = not ancestor1.eval()
             self.ancestors.append(ancestor1)
+            res = not ancestor1.eval()
             self.res = res
             return res
         
         if data == "eq":
             ancestor1 = DeriviationTreeNode(self.exp.children[0],deepcopy(self.env))
             ancestor2 = DeriviationTreeNode(self.exp.children[1],deepcopy(self.env))
-            res = ancestor1.eval() == ancestor2.eval()
             self.ancestors.append(ancestor1)
             self.ancestors.append(ancestor2)
+            res = ancestor1.eval() == ancestor2.eval()
             self.res = res
             return res
         
         if data == "lt":
             ancestor1 = DeriviationTreeNode(self.exp.children[0],deepcopy(self.env))
             ancestor2 = DeriviationTreeNode(self.exp.children[1],deepcopy(self.env))
-            res = ancestor1.eval() < ancestor2.eval()
             self.ancestors.append(ancestor1)
             self.ancestors.append(ancestor2)
+            res = ancestor1.eval() < ancestor2.eval()
             self.res = res
             return res
 
         print (data)
         raise Exception("Unknown token type")
 
-def tree_to_string(tree:Union[ParseTree,Token]):
-    
-    if isinstance(tree, Token):
-        return tree.value
-    
-    data = tree.data
-    is_aexp = data in ["aexp","term","factor","add","sub","mul","call"]
-    is_bexp = data in ["bexp","batom","and","or","not","eq","lt"]
-    is_com = data in ["com","skip","assign","ifelse","while","seq","print","def"]
-    
-    if is_aexp:
-        return aexp_tree_to_string(tree)
-    if is_bexp:
-        return bexp_tree_to_string(tree)
-    if is_com:
-        return com_tree_to_string(tree)
-    
-    print (data)
-    raise Exception("Unknown token type")
-
-def aexp_tree_to_string(tree:Union[ParseTree,Token]):
-    
-    if isinstance(tree, Token):
-        return tree.value
-    
-    data = tree.data
-    
-    if data == "add":
-        return aexp_tree_to_string(tree.children[0]) + "+" + aexp_tree_to_string(tree.children[1])
-    elif data == "sub":
-        return aexp_tree_to_string(tree.children[0]) + "-" + aexp_tree_to_string(tree.children[1])
-    elif data == "mul":
-        return "(" + aexp_tree_to_string(tree.children[0]) + ")*(" + aexp_tree_to_string(tree.children[1]) + ")"
-    elif data == "aexp":
-        return "" + aexp_tree_to_string(tree.children[0]) + ""
-    elif data == "term":
-        return "" + aexp_tree_to_string(tree.children[0]) + ""
-    elif data == "factor":
-        return "" + aexp_tree_to_string(tree.children[0]) + ""
-    
-    if data == "call":
-        funcname = tree.children[0].value
-        args = tree.children[1:]
-        args_str = " ".join([aexp_tree_to_string(arg) for arg in args])
-        
-        return funcname + "(" + args_str + ")"
-
-    print (data)
-    
-    raise Exception("Unknown token type")
-            
-def bexp_tree_to_string(tree:Union[ParseTree,Token]):
-    
-    if isinstance(tree, Token):
-        return tree.value
-    
-    data = tree.data
-    
-    if data == "and":
-        return "(" + bexp_tree_to_string(tree.children[0]) + ")and(" + bexp_tree_to_string(tree.children[1]) + ")"
-    elif data == "or":
-        return "(" + bexp_tree_to_string(tree.children[0]) + ")or(" + bexp_tree_to_string(tree.children[1]) + ")"
-    elif data == "not":
-        return "not(" + bexp_tree_to_string(tree.children[0]) + ")"
-    
-    elif data == "bexp":
-        return "" + bexp_tree_to_string(tree.children[0]) + ""
-    elif data == "batom":
-        return "" + bexp_tree_to_string(tree.children[0]) + ""
-    elif data == "bnot":
-        return "" + bexp_tree_to_string(tree.children[0]) + ""
-    elif data == "bor":
-        return "" + bexp_tree_to_string(tree.children[0]) + ""
-    elif data == "band":
-        return "" + bexp_tree_to_string(tree.children[0]) + ""
-    elif data == "eq":
-        return "" + aexp_tree_to_string(tree.children[0]) + "=" + aexp_tree_to_string(tree.children[1]) + ""
-    elif data ==  "lt":
-        return "(" + aexp_tree_to_string(tree.children[0]) + "<" + aexp_tree_to_string(tree.children[1]) + ")"
-    print (tree.pretty())
-    raise Exception("Unknown token type")
-
-def com_tree_to_string(tree:Union[ParseTree,Token]):
-    
-    if isinstance(tree, Token):
-        return "skip"
-    
-    data = tree.data
-    
-    if data == "skip":
-        return "skip"
-    elif data == "assign":
-        return "" + tree.children[0].value + ":=" + aexp_tree_to_string(tree.children[1]) + ""
-    elif data == "ifelse":
-        return "if " + bexp_tree_to_string(tree.children[0]) + " then " + com_tree_to_string(tree.children[1]) + " else " + com_tree_to_string(tree.children[2]) + "end"
-    elif data == "seq":
-        return "" + com_tree_to_string(tree.children[0]) + ";" + com_tree_to_string(tree.children[1]) + ""
-    elif data == "while":
-        return "while " + bexp_tree_to_string(tree.children[0]) + " do " + com_tree_to_string(tree.children[1]) + "end"
-    elif data == "com":
-        return "" + com_tree_to_string(tree.children[0]) + ""
-    elif data == "print":
-        return "print " + aexp_tree_to_string(tree.children[0]) + ""
-    
-    if data == "def":
-        func_name = tree.children[0].value
-        args = tree.children[1:-2]
-        args_str = ""
-        for arg in args:
-            args_str += arg.value + " "
-        com_str = com_tree_to_string(tree.children[-2])
-        returned_aexp_str = aexp_tree_to_string(tree.children[-1])
-        return "def "+func_name+"{...}"
-        return "def " + func_name + "(" + args_str + "){" + com_str + "; return " + returned_aexp_str + "}"
-    
-    print (tree.pretty())
-    raise Exception("Unknown token type")
-
-
 
 
 def run_code (code : str) :
-    IMP_grammar = ""
-
-    with open("syntax.lark", "r") as f:
-        IMP_grammar = f.read()
-
-    parser = Lark(IMP_grammar, start='com', parser='lalr',propagate_positions=True)
-    try:
-        parse_tree = parser.parse(code)
-    except Exception as e:
-
-        import traceback
-        traceback.print_exc(1)
-
-        exit (1)
-    simplified_tree = RemoveRedundant().transform(parse_tree)
     
+    simplified_tree = constract_ast(code)
     print ("constructing deriviation tree...")
     
+    from collections import Counter
+    eval_count_for_line = Counter()
+    
+    def count_nodes(tree):
+        eval_count_for_line[tree.exp.line] += 1
+        return 1 + sum([count_nodes(child) for child in tree.ancestors])
+    
+    def print_profile():
+        print ("eval frequency for each line")
+        # sort
+        eval_count_for_line_list = sorted(eval_count_for_line.items(), key=lambda x:-x[1])
+        total = sum(eval_count_for_line.values())
+        for line, count in eval_count_for_line_list:
+            print ("line {} : {} ({:.2f}%)".format(line,count,count/total*100))
+            
+        return
+    
     deriviation_tree = DeriviationTreeNode(simplified_tree,Env())
-    res = deriviation_tree.eval()
-    
+    try:
+        res = deriviation_tree.eval()
+    except KeyboardInterrupt:
+        print ("Keyboard Interruption!!!")
+        print ("number of nodes in deriviation tree (aborted) : {}".format(count_nodes(deriviation_tree)))
+        
+        print_profile()
+        exit ()
+        
     print ("finished constructing deriviation tree")
+
+    print ("number of nodes in deriviation tree : {}".format(count_nodes(deriviation_tree)))
     
+    print_profile()
+
     return deriviation_tree
 
 
