882b0d6afc6c62054eefc247bb409b21cc1c7a2e
ryoryon66 <>
2023-05-15 13:23:12 +0900

    rename

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
-# 引数でlog fileをつくるかどうかを指定する
-# 例: ./compile.sh 1
+# 引数でプログラムへのパスを受け取り、コンパイルして実行する
+# 例: ./compile.sh program_path 
+
+# programファイルへのパスをコマンドライン引数から取得
+program_path=$1
 
 
-
-
-
-python compiler.py > simple_asm_with_label.txt
+python compiler.py  $program_path > simple_asm_with_label.txt
 python resolve_address.py --input simple_asm_with_label.txt --output simple_asm.txt
 
 echo "---------------------" 
@@ -14,20 +14,15 @@ java -jar SimpleSimulator.jar   -m  simple_asm.txt
 
 echo "---------------------"
 
-# log fileをつくるかどうか
-
-if [ $# -eq 0 ]; then
-    echo "fin. did not make log file"
-    exit 1
-fi
 
 
-# 標準エラー出力をファイルにリダイレクト
-java -jar SimpleSimulator.jar  -d -m  simple_asm.txt 2> log.txt
 
-echo "---------------------" 
-echo "in order to see record log, please type the same value when Input? is asked"
+# # 標準エラー出力をファイルにリダイレクト
+# java -jar SimpleSimulator.jar  -d -m  simple_asm.txt 2> log.txt
 
-echo "---------------------" >> log.txt
-# 追記
-java -jar SimpleSimulator.jar   -m  simple_asm.txt >> log.txt
+# echo "---------------------" 
+# echo "in order to see record log, please type the same value when Input? is asked"
+
+# echo "---------------------" >> log.txt
+# # 追記
+# java -jar SimpleSimulator.jar   -m  simple_asm.txt >> log.txt
